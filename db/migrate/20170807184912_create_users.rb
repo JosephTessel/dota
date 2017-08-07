f5a198da2a8b27b8db1df73dd482ae6a3a7d60ef
Joseph Tessel <Tessel.Joseph@gmail.com>
2017-08-07 14:53:37 -0400

    Altering User model

--- db/migrate/20170807184912_create_users.rb
+++ db/migrate/20170807184912_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      add_index :users, :uid, unique: true
       t.string :uid
       t.string :nickname
       t.string :avatar_url
